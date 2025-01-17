f6a501b10a8ebfbec9c178809de41e1461bb4bc8
sinophpscript <sinophpscript@gmail.com>
2025-01-17 09:18:17 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course by VERCEL and not by Maximillian!</h1>
-      <p>The weird thing about this URL is that, it end with .app</p>
+      <h1>Welcome to Mike NextJS Vercel</h1>
+      <p>This URL end with .app</p>
     </main>
   );
 }
