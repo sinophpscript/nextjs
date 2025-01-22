4fa09fb600537d306dcd4aeaec0b04e76a561b58
sinophpscript <sinophpscript@gmail.com>
2025-01-23 00:44:19 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,23 @@ export default function Home() {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to Mike NextJS Vercel</h1>
       <p>This URL end with .app</p>
+
+    
+    <section class="intro">
+            <h2>About This Page</h2>
+            <p>This is a simple example to show how to integrate CSS3 into an HTML file.</p>
+        </section>
+
+        <section class="features">
+            <h2>Features</h2>
+            <ul>
+                <li>Responsive Design</li>
+                <li>CSS3 Animations</li>
+                <li>Flexible Layout</li>
+            </ul>
+        </section>
+   
+    
     </main>
   );
 }
